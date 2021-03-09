6824d196e1577550c142c219a880f61c7dd7fdff
reityeru <reityeru0084055@gmail.com>
2021-03-09 15:17:56 +0900

    main_userバリデーションの設定

--- app/models/main_user.rb
+++ app/models/main_user.rb
@@ -3,4 +3,25 @@ class MainUser < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+    validates :nickname
+    validates :birthday
+    validates :email, format: { with: /\A\S+@\S+\.\S+\z/ }
+    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
+    validates :password_confirmation
+    validates :gender
+    validates :phone_number, format: { with: /\A\d{10,11}\z/ }
+    
+      
+    with_options format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/ } do
+      validates :last_name
+      validates :first_name
+    end
+      
+    with_options format: { with: /\A[ァ-ヶー－]+\z/ } do
+      validates :last_name_kana
+      validates :first_name_kana
+    end
+  end
 end
