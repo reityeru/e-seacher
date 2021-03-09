8e1aae3421b0eb5f3ff4b025d2f56db81f77341f
reityeru <reityeru0084055@gmail.com>
2021-03-09 15:30:23 +0900

    バリデーション、アソシエーション設定

--- app/models/shop_user.rb
+++ app/models/shop_user.rb
@@ -3,4 +3,19 @@ class ShopUser < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+    validates :shop_name
+    validates :prefectures
+    validates :city
+    validates :address
+    validates :password_confirmation
+    validates :phone_number, format: { with: /\A\d{10,11}\z/ }
+  end
+
+  has_many :shop_user_tweets
+  has_many :payment_types, through: :shop_user_payment_type
+  has_many :shop_user_payment_types
+  has_many :main_user_tweets
+  has_many :shop_user_comments
 end
