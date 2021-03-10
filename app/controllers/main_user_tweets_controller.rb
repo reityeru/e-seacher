b28c9698070bd731f8a807541ce29e215615f8f2
reityeru <reityeru0084055@gmail.com>
2021-03-10 22:57:55 +0900

    main_user_tweetsコントローラー作成

--- app/controllers/main_user_tweets_controller.rb
+++ app/controllers/main_user_tweets_controller.rb
@@ -1,2 +1,22 @@
 class MainUserTweetsController < ApplicationController
+  def new
+    @main_user_tweet = MainUserTweet.new
+  end
+
+  def create
+    @main_user_tweet = MainUserTweet.new(main_user_tweet_params)
+    if @main_user_tweet.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+
+
+
+  private
+  def main_user_tweet_params
+    params.require(:main_user_tweet).permit(:shop_name, :text, :prefectures, :city, :genres_id, :take_out, :shop_user, payment_type_ids:[]).merge(main_user_id: current_main_user.id)
+  end
 end
