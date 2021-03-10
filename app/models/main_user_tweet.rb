9f52af639546f997f3f7c0dafaa5773fe2df6af6
reityeru <reityeru0084055@gmail.com>
2021-03-10 22:59:06 +0900

    main_user_tweetモデルバリデーション設定

--- app/models/main_user_tweet.rb
+++ app/models/main_user_tweet.rb
@@ -1,2 +1,24 @@
 class MainUserTweet < ApplicationRecord
+  with_options presence: true do
+    validates :shop_name
+    validates :text, length: { maximum: 300 }
+    validates :prefectures
+    validates :city
+    validates :genres_id
+    validates :take_out
+  end
+
+
+  belongs_to :main_user
+  belongs_to :shop_user, optional: true
+  has_many   :main_user_comments
+  has_many   :shop_user_comments
+  has_many   :goods
+  has_many   :likes
+  has_many   :main_user_tweet_payment_types
+  has_many   :payment_types, through: :main_user_tweet_payment_types
+  has_many   :main_user_tweet_images
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :genre
 end
