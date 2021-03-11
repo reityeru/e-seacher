98675b8907348a5cee4c73b3fea993e0e85a14e0
reityeru <reityeru0084055@gmail.com>
2021-03-11 11:27:39 +0900

    main_user_tweetにshop_userを紐つける処理実装

--- app/controllers/main_user_tweets_controller.rb
+++ app/controllers/main_user_tweets_controller.rb
@@ -5,13 +5,22 @@ class MainUserTweetsController < ApplicationController
 
   def create
     @main_user_tweet = MainUserTweet.new(main_user_tweet_params)
-    if @main_user_tweet.save
-      redirect_to root_path
+    shop_user = ShopUser.find_by(shop_name: @main_user_tweet.shop_name)
+    if shop_user.nil?
+      if @main_user_tweet.save
+        redirect_to root_path
+      else
+        render :new
+      end
     else
-      render :new
+      @main_user_tweet.shop_user_id = shop_user.id
+      if @main_user_tweet.save
+        redirect_to root_path
+      else
+        render :new
+      end
     end
   end
-
 
 
 
