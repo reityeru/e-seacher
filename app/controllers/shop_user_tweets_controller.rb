e05998822a6c3f3f30c59e3e911063dfbd8ba780
reityeru <reityeru0084055@gmail.com>
2021-03-11 10:51:25 +0900

    shop_user_tweetsコントローラーcreateアクション設定

--- app/controllers/shop_user_tweets_controller.rb
+++ app/controllers/shop_user_tweets_controller.rb
@@ -2,4 +2,22 @@ class ShopUserTweetsController < ApplicationController
   def new
     @shop_user_tweet = ShopUserTweet.new
   end
+
+  def create
+    @shop_user_tweet = ShopUserTweet.new(shop_user_tweet_params)
+    if @shop_user_tweet.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+
+
+
+  private
+  def shop_user_tweet_params
+    params.require(:shop_user_tweet).permit(:text).merge(shop_user_id: current_shop_user.id)
+  end
+  
 end
