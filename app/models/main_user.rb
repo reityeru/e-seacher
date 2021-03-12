dd8151fbf414d3970861f457090377bd5fe17d0d
reityeru <reityeru0084055@gmail.com>
2021-03-12 15:35:27 +0900

    未実装箇所のdependent: :destroyの削除

--- app/models/main_user.rb
+++ app/models/main_user.rb
@@ -26,7 +26,7 @@ class MainUser < ApplicationRecord
   validates :profile, length: { maximum: 300 }
 
   has_many :main_user_tweets, dependent: :destroy
-  has_many :goods, dependent: :destroy
-  has_many :likes, dependent: :destroy
-  has_many :main_user_comments, dependent: :destroy
+  has_many :goods
+  has_many :likes
+  has_many :main_user_comments
 end
--- app/models/shop_user_tweet.rb
+++ app/models/shop_user_tweet.rb
@@ -2,7 +2,7 @@ class ShopUserTweet < ApplicationRecord
   validates :text, presence: true, length: { maximum: 300 }
 
   belongs_to :shop_user
-  has_many   :shop_user_comments, dependent: :destroy
-  has_many   :main_user_comments, dependent: :destroy
+  has_many   :shop_user_comments
+  has_many   :main_user_comments
   has_many_attached :shop_user_tweet_images
 end
