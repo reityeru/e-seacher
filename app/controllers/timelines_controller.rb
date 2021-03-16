92bfe5f2737a45e6859d357fc4549adecc37effc
reityeru <reityeru0084055@gmail.com>
2021-03-16 18:18:10 +0900

    スコープの設定

--- app/controllers/timelines_controller.rb
+++ app/controllers/timelines_controller.rb
@@ -13,7 +13,7 @@ class TimelinesController < ApplicationController
     prefecture_id = params[:prefecture_id]
     city_id = params[:city_id]
     genre_id = params[:genre_id]
-    selected_payment_type_ids = selected_payment_type_params
+    selected_payment_type_ids = params[:payment_type_ids]
     selected_main_user_tweet_ids = MainUserTweetPaymentType.where(payment_type_id: selected_payment_type_ids).pluck(:main_user_tweet_id)
     take_out = params[:take_out]
     # 投稿検索結果
@@ -29,16 +29,11 @@ class TimelinesController < ApplicationController
   # 投稿検索
   def main_user_tweets(keyword, prefecture_id, city_id, genre_id, selected_main_user_tweet_ids, take_out)
     # 投稿検索結果
-    MainUserTweet.where('shop_name LIKE ?', "%#{keyword}%").where(prefecture_id: prefecture_id).where(city_id: city_id).where(genre_id: genre_id).where(id: selected_main_user_tweet_ids).where(take_out: take_out)
+    MainUserTweet.shop_name(keyword).prefecture_id(prefecture_id).city_id(city_id).genre_id(genre_id).main_user_tweet_id(selected_main_user_tweet_ids).take_out(take_out)
   end
   
   # 投稿検索
   def shop_user_tweets(keyword, prefecture_id, city_id)
     ShopUserTweet.joins(:shop_user).where('shop_name LIKE ?', "%#{keyword}%").where(shop_users: {prefecture_id: prefecture_id}).where(shop_users: {city_id: city_id})
   end
-
-  def selected_payment_type_params
-    params.require(:payment_type_ids)
-  end
-
 end
--- app/models/main_user_tweet.rb
+++ app/models/main_user_tweet.rb
@@ -19,6 +19,15 @@ class MainUserTweet < ApplicationRecord
   has_many   :payment_types, through: :main_user_tweet_payment_types
   has_many_attached :main_user_tweet_images
 
+  # 検索フォームのスコープ
+  scope :shop_name, -> (keyword) { where('shop_name LIKE ?', "%#{keyword}%") }
+  scope :prefecture_id, -> (prefecture_id) { where(prefecture_id: prefecture_id) }
+  scope :city_id, -> (city_id) { where(city_id: city_id) }
+  scope :genre_id, -> (genre_id) { where(genre_id: genre_id) }
+  scope :main_user_tweet_id, -> (selected_main_user_tweet_ids) { where(id: selected_main_user_tweet_ids) if selected_main_user_tweet_ids.present? }
+  scope :take_out, -> (take_out) { where(take_out: take_out) if take_out.present? }
+
+
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :genre
 end
