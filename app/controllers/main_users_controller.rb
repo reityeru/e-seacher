1ab2cdf3aed69fa3e3d3ff74b904d55d422d7e59
reityeru <reityeru0084055@gmail.com>
2021-03-17 18:53:49 +0900

    main_userのupdateアクションの設定

--- app/controllers/main_users_controller.rb
+++ app/controllers/main_users_controller.rb
@@ -8,5 +8,18 @@ class MainUsersController < ApplicationController
     @main_user = MainUser.find(params[:id])
   end
 
+  def update
+    @main_user = MainUser.find(params[:id])
+    if @main_user.update(main_user_params)
+      redirect_to main_user_path(current_main_user.id)
+    else
+      render :edit
+    end
+  end
 
+
+  private
+  def main_user_params
+    params.require(:main_user).permit(:nickname, :phone_number, :profile)
+  end
 end
