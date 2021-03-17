ae4a9a0fba709f7e7bb4444a1d073ef2a6dc6798
reityeru <reityeru0084055@gmail.com>
2021-03-17 20:41:30 +0900

    shop_userのdestoryアクション設定、共通処理の統一

--- app/controllers/shop_users_controller.rb
+++ app/controllers/shop_users_controller.rb
@@ -1,20 +1,25 @@
 class ShopUsersController < ApplicationController
+  before_action :set_shop_user, only: [:show, :edit, :update, :destroy]
+  before_action :move_to_index, only: [:edit, :update, :destroy]
+
   def show
-    @shop_user = ShopUser.find(params[:id])
     @shop_user_tweets = @shop_user.shop_user_tweets
   end
 
   def edit
-    @shop_user = ShopUser.find(params[:id])
   end
 
   def update
-    @shop_user = ShopUser.find(params[:id])
     if @shop_user.update(shop_user_params)
       redirect_to shop_user_path(current_shop_user.id)
     else
       render :edit
     end
+  end
+
+  def destroy
+    @shop_user.destroy
+    redirect_to new_main_user_session_path
   end
 
 
@@ -23,4 +28,15 @@ class ShopUsersController < ApplicationController
     params.require(:shop_user).permit(:shop_name, :phone_number, :postal_code, :prefecture_id, :city_id, :address, :building_name, :profile, :shop_link, payment_type_ids: [])
   end
 
+  def set_shop_user
+    @shop_user = ShopUser.find(params[:id])
+  end
+
+  def move_to_index
+    if shop_user_signed_in?
+      redirect_to root_path if current_shop_user.id != @shop_user.id
+    else
+      redirect_to root_path
+    end
+  end
 end
