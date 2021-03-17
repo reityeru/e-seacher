ed06a333829d48bf27db7e555ed39a46a0942535
reityeru <reityeru0084055@gmail.com>
2021-03-17 18:24:30 +0900

    main_userのeditアクション設定

--- app/controllers/main_users_controller.rb
+++ app/controllers/main_users_controller.rb
@@ -3,4 +3,10 @@ class MainUsersController < ApplicationController
     @main_user = MainUser.find(params[:id])
     @main_user_tweets = @main_user.main_user_tweets
   end
+
+  def edit
+    @main_user = MainUser.find(params[:id])
+  end
+
+
 end
