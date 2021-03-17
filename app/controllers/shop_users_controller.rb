d86f81363cccc01f7553d9a86a469952a3aa4557
reityeru <reityeru0084055@gmail.com>
2021-03-17 19:08:34 +0900

    shop_userのedit,updateアクションの設定

--- app/controllers/shop_users_controller.rb
+++ app/controllers/shop_users_controller.rb
@@ -3,4 +3,24 @@ class ShopUsersController < ApplicationController
     @shop_user = ShopUser.find(params[:id])
     @shop_user_tweets = @shop_user.shop_user_tweets
   end
+
+  def edit
+    @shop_user = ShopUser.find(params[:id])
+  end
+
+  def update
+    @shop_user = ShopUser.find(params[:id])
+    if @shop_user.update(shop_user_params)
+      redirect_to shop_user_path(current_shop_user.id)
+    else
+      render :edit
+    end
+  end
+
+
+  private
+  def shop_user_params
+    params.require(:shop_user).permit(:shop_name, :phone_number, :postal_code, :prefecture_id, :city_id, :address, :building_name, :profile, :shop_link, payment_type_ids: [])
+  end
+
 end
