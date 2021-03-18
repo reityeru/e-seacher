0fc5f2e9ea21e40ebee6a7cea9daf9275b0ecd0b
reityeru <reityeru0084055@gmail.com>
2021-03-18 19:04:02 +0900

    アソシエーションの修正

--- app/models/main_user_tweet.rb
+++ app/models/main_user_tweet.rb
@@ -11,10 +11,8 @@ class MainUserTweet < ApplicationRecord
   belongs_to :shop_user, optional: true
   belongs_to :prefecture
   belongs_to :city
-  has_many   :main_user_comments
-  has_many   :shop_user_comments
-  has_many   :goods
-  has_many   :likes
+  has_many   :main_user_comments, dependent: :destroy
+  has_many   :shop_user_comments, dependent: :destroy
   has_many   :main_user_tweet_payment_types, dependent: :destroy
   has_many   :payment_types, through: :main_user_tweet_payment_types
   has_many_attached :main_user_tweet_images
