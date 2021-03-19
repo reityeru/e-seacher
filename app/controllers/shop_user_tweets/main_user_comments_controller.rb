4542f17178a933c6611c153bfd42466278d35192
reityeru <reityeru0084055@gmail.com>
2021-03-19 18:31:27 +0900

    ポリモーフィック関連付けに対応したコントローラー作成

---
+++ app/controllers/shop_user_tweets/main_user_comments_controller.rb
@@ -0,0 +1,7 @@
+class ShopUserTweet::MainUserCommentsController < MainUserCommentsController  
+  before_action :set_main_user_commentable, only: :create  
+  private  
+    def set_main_user_commentable  
+      @main_user_commentable = ShopUserTweet.find(params[:shop_user_tweet_id])  
+    end  
+end  
