fee6381b170818de6211be1cc5e82d9807505f81
reityeru <reityeru0084055@gmail.com>
2021-03-19 15:35:47 +0900

    バリデーションの設定

--- app/models/main_user_comment.rb
+++ app/models/main_user_comment.rb
@@ -1,4 +1,11 @@
 class MainUserComment < ApplicationRecord
+  with_options presence: true do
+    validates :comment, length: { maximum: 300 }
+    validates :main_usesr_tweet_commentable_id
+    validates :main_usesr_tweet_commentable_type
+  end
+
+
   belongs_to :main_user
   belongs_to :main_usesr_tweet_commentable,  polymorphic: true
 end
