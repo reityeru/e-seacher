4c3520a5a7b4b19a3e7a930777a2e115a2343895
reityeru <reityeru0084055@gmail.com>
2021-03-20 15:49:20 +0900

    main_user_commentのcresteアクション修正

--- app/controllers/main_user_comments_controller.rb
+++ app/controllers/main_user_comments_controller.rb
@@ -5,20 +5,20 @@ class MainUserCommentsController < ApplicationController
     @main_user_comment = @main_user_commentable.main_user_comments.build(main_user_comment_params)
     if @main_user_commentable.class == MainUserTweet
       if @main_user_comment.save 
-        redirect_to main_user_tweet_path(@main_user_comment.main_user_tweet)
+        redirect_to main_user_tweet_path(@main_user_commentable.id)
       else
-        @main_user_tweet = @main_user_comment.main_user_tweet
+        @main_user_tweet = @main_user_commentable
         main_user_comments = @main_user_tweet.main_user_comments
-        shop_user_comments = @main_user_tweet.shop_user_comments
-        @comments = main_user_comments | shop_user_comments
-        @comments.sort! { |a, b| b.created_at <=> a.created_at }
+        # shop_user_comments = @main_user_tweet.shop_user_comments
+        @comments = main_user_comments # | shop_user_comments
+        #@comments.sort! { |a, b| b.created_at <=> a.created_at }
         render "main_user_tweets/show"
       end
     else
       if @main_user_comment.save 
         redirect_to shop_user_tweet_path(@main_user_comment.shop_user_tweet)
       else
-        @shop_user_tweet = @main_user_comment.shop_user_tweet
+        @shop_user_tweet = @main_user_commentable
         main_user_comments = @main_user_tweet.main_user_comments
         shop_user_comments = @main_user_tweet.shop_user_comments
         @comments = main_user_comments | shop_user_comments
@@ -32,7 +32,7 @@ class MainUserCommentsController < ApplicationController
 
   private
   def main_user_comment_params
-    params.require(:main_user_comment).permit(:text).merge(main_user_id: current_main_user.id)
+    params.require(:main_user_comment).permit(:comment).merge(main_user_id: current_main_user.id)
   end
 
 
