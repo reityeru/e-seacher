30bf137f9abc468340d10f65c22fdb7174038df3
reityeru <reityeru0084055@gmail.com>
2021-03-22 17:06:19 +0900

    main_user_commentモデルテストコード作成（未完成）

--- spec/factories/main_user_comments.rb
+++ spec/factories/main_user_comments.rb
@@ -1,5 +1,18 @@
 FactoryBot.define do
   factory :main_user_comment do
-    
+    factory :main_user_tweet_main_user_comment do
+      association :main_user
+      association :main_usesr_tweet_commentable, factory: :main_user_tweet
+      commentable_type { 'MainUserTweet' }
+      comment          { 'テストコメント' }
+    end
+
+    factory :shop_user_tweet_main_user_comment do
+      association :main_user
+      association :main_usesr_tweet_commentable, factory: :main_user_tweet
+      commentable_type { 'ShopUserTweet' }
+      comment          { 'テストコメント' }
+    end
+
   end
 end
--- spec/models/main_user_comment_spec.rb
+++ spec/models/main_user_comment_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe MainUserComment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+    let(:main_user_tweet_main_user_comment) { FactoryBot.create(:main_user_tweet_main_user_comment) }
+    let(:shop_user_tweet_main_user_comment) { FactoryBot.create(:shop_user_tweet_main_user_comment) }
+
+  context '保存できる場合' do
+    describe '一般ユーザーコメント' do
+      it '全ての項目が存在すれば一般ユーザーの投稿にコメントできること' do
+        expect(main_user_tweet_main_user_comment).to be_valid
+      end
+    end
+  end
 end
